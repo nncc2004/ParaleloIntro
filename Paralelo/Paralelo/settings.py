5088ede9fbd13bbfd728eb732c20023e6fc769b0
Nicolas Chehade <nncc2004@gmail.com>
2023-10-11 00:48:14 -0300

    Creación de las templates y la navbar, sesión 2

--- Paralelo/Paralelo/settings.py
+++ Paralelo/Paralelo/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Paralelo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Paralelo/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
