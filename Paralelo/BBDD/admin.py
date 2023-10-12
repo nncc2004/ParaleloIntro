27e7da71d9b1b8ec84bda47804994788db4b1460
Nicolas Chehade <nncc2004@gmail.com>
2023-10-12 13:28:25 -0300

    eliminación de Id de cada tabla en específico

--- Paralelo/BBDD/admin.py
+++ Paralelo/BBDD/admin.py
@@ -3,23 +3,23 @@ from BBDD.models import lista_reproduccion, curso_usuario, cursos, videos
 # Register your models here.
 
 class CursoUsuarioAdmin(admin.ModelAdmin):
-    list_display = ("UserCourseId", "id_curso", "id_usuario", "progreso")
-    search_fields = ("UserCourseId", "id_curso", "id_usuario", "progreso")
+    list_display = ( "id_curso", "id_usuario", "progreso")
+    search_fields = ( "id_curso", "id_usuario", "progreso")
 
 
 class CursosAdmin(admin.ModelAdmin):
-    list_display = ("CourseId", "nombre_curso", "cantidad_videos", "autor", "privacidad")
-    search_fields = ("CourseId", "nombre_curso", "cantidad_videos", "autor", "privacidad")
+    list_display = ( "nombre_curso", "cantidad_videos", "autor", "privacidad")
+    search_fields = ( "nombre_curso", "cantidad_videos", "autor", "privacidad")
     list_filter = ("privacidad",)
 
 
 class listaAdmin(admin.ModelAdmin):
-    list_display = ("ListId", "id_curso", "id_video")
-    search_fields = ("ListId", "id_curso", "id_video")
+    list_display = ("id_curso", "id_video")
+    search_fields = ( "id_curso", "id_video")
 
 class VideoAdmin(admin.ModelAdmin):
-    list_display = ("VideoId", "nombre_video", "duracion", "url", "autor", "clasificacion")
-    search_fields = ("VideoId", "nombre_video", "duracion", "url", "autor", "clasificacion")
+    list_display = ( "nombre_video", "duracion", "url", "autor", "clasificacion")
+    search_fields = ( "nombre_video", "duracion", "url", "autor", "clasificacion")
     list_filter = ("autor", "clasificacion", )
 
 
--- Paralelo/BBDD/models.py
+++ Paralelo/BBDD/models.py
@@ -2,34 +2,30 @@ from django.db import models
 
 # Create your models here.
 class curso_usuario(models.Model):
-    UserCourseId = models.IntegerField(verbose_name= "ID")
     id_curso = models.IntegerField()
     id_usuario = models.IntegerField()
     progreso = models.IntegerField()
     def __str__(self):
-        return "ID: %s. IdCurso: %s. IdUsuario: %s. Progreso: %s." %(self.UserCourseId, self.id_curso, self.id_usuario, self.progreso)
+        return "IdCurso: %s. IdUsuario: %s. Progreso: %s." %(self.id_curso, self.id_usuario, self.progreso)
 
 class cursos(models.Model):
-    CourseId = models.IntegerField(verbose_name= "ID")
     nombre_curso = models.CharField(max_length=100)
     cantidad_videos = models.IntegerField()
     autor = models.CharField(max_length=100)
     privacidad = models.BooleanField()
     def __str__(self):
-        return "ID: %s. Nombre_Curso: %s. Cantidad_Videos: %s. Autor: %s. Privacidad: %s." %(self.CourseId, self.nombre_curso, self.cantidad_videos, self.autor, self.privacidad)
+        return "Nombre_Curso: %s. Cantidad_Videos: %s. Autor: %s. Privacidad: %s." %( self.nombre_curso, self.cantidad_videos, self.autor, self.privacidad)
 
 class lista_reproduccion(models.Model):
-    ListId = models.IntegerField(verbose_name= "ID")
     id_curso = models.IntegerField()
     id_video = models.IntegerField()
     def __str__(self):
-        return "ID: %s. IdCurso: %s. IdVideo: %s." %(self.ListId, self.id_curso, self.id_video)
+        return "IdCurso: %s. IdVideo: %s." %(self.id_curso, self.id_video)
 class videos(models.Model):
-    VideoId = models.IntegerField(verbose_name= "ID")
     nombre_video = models.CharField(max_length=100)
     duracion = models.CharField(max_length=100)
     url = models.URLField()
     autor = models.CharField(max_length=100)
     clasificacion = models.CharField(max_length=100)
     def __str__(self):
-        return "ID: %s. Nombre_video: %s. Duración: %s. URL: %s. Autor: %s, Clasificación: %s" %(self.VideoId, self.nombre_video, self.duracion, self.url, self.autor, self.clasificacion)
+        return "Nombre_video: %s. Duración: %s. URL: %s. Autor: %s, Clasificación: %s" %(self.nombre_video, self.duracion, self.url, self.autor, self.clasificacion)
